8fb11c9f529cd41d603b866bc680997783678461
kiran Buddha <buddhasuryakiran@gmail.com>
2020-07-04 22:50:00 +0530

    changes ad done

--- examples/yaml-example.js
+++ examples/yaml-example.js
@@ -3,11 +3,12 @@ const k8s = require('@kubernetes/client-node');
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+iconst k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
 const yamlString = k8s.dumpYaml({
   metadata: {
     name: 'test'
+	  name:'kiran'
   }
 });
 
